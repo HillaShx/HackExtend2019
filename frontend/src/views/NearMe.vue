49ccd4019c2802dbec225d92c6464f86367dd1a3
Sapir Malka <sapirm290@gmail.com>
2019-11-28 20:33:02 +0200

    buttons for chossing material types are implemented

--- frontend/src/views/NearMe.vue
+++ frontend/src/views/NearMe.vue
@@ -2,9 +2,13 @@
   <div>
     <h1>נקודות מחזור מסביבי</h1>
     <v-btn small @click="closePaper" class="matBtn" v-bind:class="{ active: isPaperOpen }">נייר</v-btn>
-    <v-btn small class="matBtn">קרטון</v-btn>
-    <v-btn small class="matBtn">זכוכית</v-btn>
-    <v-btn small class="matBtn">פלסטיק</v-btn>
+    <v-btn small @click="closeGlass" class="matBtn" v-bind:class="{ active: isGlassOpen }">זכוכית</v-btn>
+    <v-btn
+      small
+      @click="closePlastic"
+      class="matBtn"
+      v-bind:class="{ active: isPlasticOpen }"
+    >פלסטיק</v-btn>
     <google-map />
   </div>
 </template>
@@ -16,12 +20,24 @@ import { store, mutations } from "@/store.js";
 export default {
   computed: {
     isPaperOpen() {
-      return !store.materials["paper"];
+      return store.materials["paper"];
+    },
+    isGlassOpen() {
+      return store.materials["glass"];
+    },
+    isPlasticOpen() {
+      return store.materials["plastic"];
     }
   },
   methods: {
     closePaper: () => {
       mutations.toggleMaterial("paper");
+    },
+    closeGlass: () => {
+      mutations.toggleMaterial("glass");
+    },
+    closePlastic: () => {
+      mutations.toggleMaterial("plastic");
     }
   },
   name: "NearMe",
