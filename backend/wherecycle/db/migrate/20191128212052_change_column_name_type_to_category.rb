f4245be5289bcce53d527a9a022280d6748c3d8a
HillaShx <hilla.sh@gmail.com>
2019-11-29 02:51:38 +0200

    index api end-point is working (retreiving json)

--- backend/wherecycle/db/migrate/20191128212052_change_column_name_type_to_category.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameTypeToCategory < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :recycle_points, :type, :category
-  end
-end
--- backend/wherecycle/db/migrate/20191128212601_add_user_to_recycle_points.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToRecyclePoints < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :recycle_points, :user, index: true
-    add_foreign_key :recycle_points, :users
-  end
-end
